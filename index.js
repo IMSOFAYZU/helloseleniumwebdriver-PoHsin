439ff777b9f3caaa60dca2b160f7c0fb1f8381cc
PoHsin <ok118023@gmail.com>
2019-07-19 15:13:13 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const DRIVER="geckodriver"; //chromedriver
-const BROWSER="firefox"; //chrome
+const DRIVER="chromedriver"; //chromedriver
+const BROWSER="chrome"; //chrome
 
 var webdriver = require("selenium-webdriver");
 require(DRIVER);
@@ -8,3 +8,22 @@ var builder = new webdriver.Builder();
 builder.forBrowser(BROWSER);
 var driver=builder.build();
 driver.get("http://www.google.com.tw");
+
+var qLocator = webdriver.By.name("q");
+driver.wait(webdriver.until.elementLocated(qLocator),3000);
+var qElement = driver.findElement(qLocator);
+qElement.sendKeys("youtube");
+
+var submitButtonLocator = webdriver.By.name("btnK");
+driver.wait(webdriver.until.elementLocated(submitButtonLocator),3000);
+var submitButton = driver.findElement(submitButtonLocator);
+submitButton.click();
+
+// var rcLocator = webdriver.By.className("rc");
+// driver.wait(webdriver.until.elementLocated(rcLocator),10000);
+// var rcElements = driver.findElement(rcLocator);
+// for(let rcElement of rcElements)
+// {
+//     console.log(rcElement.getText());
+// }
+// driver.close();
